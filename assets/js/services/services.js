6ce98d60988adc1192dd6d4a328643f07958a113
Дмитрий Олейников <75032469+Mr-Vitoria@users.noreply.github.com>
2024-09-27 12:56:49 +0300

    Add service page

---
+++ assets/js/services/services.js
@@ -0,0 +1,238 @@
+let services = [
+  {
+    title: "Груминг мопса",
+    image:
+      "https://optim.tildacdn.com/tild6535-6234-4562-b332-663834303634/-/cover/240x240/center/center/-/format/webp/noroot.png",
+    price: "1600",
+    category: "Малые породы",
+  },
+  {
+    title: "Груминг мальтезе",
+    image:
+      "https://barbos.club/wp-content/uploads/3/4/2/342bbadf4a11e1332d32878f2f08647f.jpeg",
+    price: "1700",
+    category: "Малые породы",
+  },
+  {
+    title: "Груминг Бишона",
+    image:
+      "https://optim.tildacdn.com/tild6630-6162-4561-b865-633633353366/-/cover/240x240/center/center/-/format/webp/IMG_0365.jpeg",
+    price: "2500",
+    category: "Малые породы",
+  },
+  {
+    title: "Груминг Чихуа",
+    image:
+      "https://optim.tildacdn.com/tild3130-3631-4231-b337-636361326436/-/cover/240x240/center/center/-/format/webp/IMG_0654.jpeg",
+    price: "1400",
+    category: "Малые породы",
+  },
+
+  {
+    title: "Груминг Корги",
+    image:
+      "https://optim.tildacdn.com/tild3566-3264-4630-a531-356363363639/-/cover/240x240/center/center/-/format/webp/23098315-7b1b-4792-a.jpeg",
+    price: "1800",
+    category: "Средние породы",
+  },
+  {
+    title: "Груминг Американского Кокер Спаниеля",
+    image:
+      "https://optim.tildacdn.com/tild3031-3363-4263-a533-643062393939/-/cover/240x240/center/center/-/format/webp/IMG_1973.jpeg",
+    price: "2300",
+    category: "Средние породы",
+  },
+  {
+    title: "Груминг Кавалер Кинг Чарльза",
+    image:
+      "https://optim.tildacdn.com/tild6638-6630-4263-a339-646263346564/-/cover/240x240/center/center/-/format/webp/IMG_9565.jpeg",
+    price: "1800",
+    category: "Средние породы",
+  },
+  {
+    title: "Груминг Французского бульдога",
+    image:
+      "https://optim.tildacdn.com/tild3765-6135-4835-b061-353661623232/-/cover/240x240/center/center/-/format/webp/0ed1da37-4bdb-46da-8.jpeg",
+    price: "1800",
+    category: "Средние породы",
+  },
+  {
+    title: "Груминг Английского бульдога",
+    image:
+      "https://i.pinimg.com/736x/ec/f2/c4/ecf2c4fd26ec97d360169d62e9a6b890.jpg",
+    price: "1800",
+    category: "Средние породы",
+  },
+
+  {
+    title: "Груминг Лабрадора",
+    image:
+      "https://optim.tildacdn.com/tild3030-3234-4238-b762-303039363464/-/cover/240x240/center/center/-/format/webp/5c2696a7-4153-48a6-a.jpeg",
+    price: "2200",
+    category: "Крупные породы",
+  },
+  {
+    title: "Груминг Хаски",
+    image:
+      "https://optim.tildacdn.com/tild3335-3131-4063-b835-373161623136/-/cover/240x240/center/center/-/format/webp/0bc833ae-db6e-4b98-8.jpeg",
+    price: "2500",
+    category: "Крупные породы",
+  },
+  {
+    title: "Груминг Золотистого Ретривера",
+    image:
+      "https://avatars.mds.yandex.net/i?id=b58e37f93845ee90d775444677d2420d_l-5147134-images-thumbs&n=13",
+    price: "2700",
+    category: "Крупные породы",
+  },
+  {
+    title: "Груминг Самоеда",
+    image:
+      "https://avatars.mds.yandex.net/i?id=1ca7df087433d3257d71aa06851fc396_l-8201030-images-thumbs&n=13",
+    price: "3000",
+    category: "Крупные породы",
+  },
+  {
+    title: "Груминг Чау Чау",
+    image:
+      "https://avatars.mds.yandex.net/i?id=8f37a9e7e921e5b2f598771e829453a9_l-5874989-images-thumbs&n=13",
+    price: "3000",
+    category: "Крупные породы",
+  },
+  {
+    title: "Груминг Кане-Корсо",
+    image:
+      "https://optim.tildacdn.com/tild3536-3039-4431-b566-393436343634/-/cover/240x240/center/center/-/format/webp/b376e6f1-8f44-4d57-a.jpeg",
+    price: "2500",
+    category: "Крупные породы",
+  },
+  {
+    title: "Груминг Овчарки",
+    image:
+      "https://avatars.mds.yandex.net/i?id=4a67df3058e5442444d553a0df8fb2e7_l-5869685-images-thumbs&n=13",
+    price: "2200",
+    category: "Крупные породы",
+  },
+  {
+    title: "Груминг Аусси",
+    image: "https://artemonsalon.ru/wp-content/uploads/2016/08/IMG_4916.jpg",
+    price: "2200",
+    category: "Крупные породы",
+  },
+
+  {
+    title: "Экспресс-линька",
+    image:
+      "https://optim.tildacdn.com/tild3465-3762-4630-b232-643565333830/-/cover/240x240/center/center/-/format/webp/__.jpeg",
+    price: "1800",
+    category: "Кошки",
+  },
+  {
+    title: "Экспресс-линька Мейн-куна",
+    image:
+      "https://optim.tildacdn.com/tild3733-6539-4239-b766-303132396138/-/cover/240x240/center/center/-/format/webp/880bb32e-26b7-4fec-9.jpeg",
+    price: "2500",
+    category: "Кошки",
+  },
+
+  {
+    title: "Джек-Рассел",
+    image:
+      "https://optim.tildacdn.com/tild3562-6337-4636-a263-616339653937/-/cover/240x240/center/center/-/format/webp/3c6c7b9e-e4b1-4b44-8.jpeg",
+    price: "2000",
+    category: "Тримминг",
+  },
+  {
+    title: "Гриффон",
+    image:
+      "https://optim.tildacdn.com/tild3031-3365-4631-b936-386330653139/-/cover/240x240/center/center/-/format/webp/IMG_1131.jpeg",
+    price: "1800",
+    category: "Тримминг",
+  },
+  {
+    title: "Цвергшнауцер",
+    image:
+      "https://optim.tildacdn.com/tild3163-3665-4466-a533-353962396563/-/cover/240x240/center/center/-/format/webp/77bab738-7c95-48b3-8.jpeg",
+    price: "3000",
+    category: "Тримминг",
+  },
+  {
+    title: "Миттельшнауцер",
+    image:
+      "https://avatars.mds.yandex.net/i?id=6db2f79389bf26dc9de39bf2c0ef85b8_l-10695517-images-thumbs&n=13",
+    price: "2500",
+    category: "Тримминг",
+  },
+];
+let serviceCategories = [... new Set(services.map((service) => service.category))];
+
+let header = document.createElement("div");
+header.classList.add("header");
+
+let headerButtons = [];
+for (const category of serviceCategories) {
+  let button = getCategoryButton(category);
+  headerButtons.push(button);
+  header.appendChild(button);
+}
+
+function getCategoryButton(title) {
+  let button = document.createElement("button");
+  button.textContent = title;
+
+  button.addEventListener("click", (ev) => {
+    changeListItems(title);
+    clearHeaderButtons();
+    button.classList.add("active");
+  });
+
+  return button;
+}
+
+function clearHeaderButtons(){
+  for (const button of headerButtons) {
+    button.classList.remove("active");
+  }
+}
+
+let list = document.createElement("div");
+list.classList.add("list");
+
+function changeListItems(activeCategory) {
+  let items = services.filter((item) => item.category == activeCategory);
+
+  list.innerHTML = "";
+
+  for (const item of items) {
+    list.appendChild(getCard(item));
+  }
+}
+
+function getCard(service) {
+  let card = document.createElement("article");
+  card.classList.add("card");
+
+  card.innerHTML = `
+                        <img src="${service.image}" alt="${service.title}"/>
+                        <h3>${service.title}</h3>
+                        <p>Комплексный уход</p>
+                        <p class="price">От ${service.price} руб.</p>`;
+
+  let button = document.createElement("button");
+  button.classList.add("button");
+  button.addEventListener("click", (ev) => {
+    alert("Записано");
+  });
+  button.textContent = "Записаться";
+
+  card.appendChild(button);
+  return card;
+}
+
+const serviceSection = document.getElementById("serviceSection");
+
+serviceSection.appendChild(header);
+serviceSection.appendChild(list);
+
+changeListItems(serviceCategories[0]);
+headerButtons[0].classList.add("active");
